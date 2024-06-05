34af3fa61e52998b41018eed7778bf04c7249e73
Umut Baybece <umut.baybece@kod.com.tr>
2024-06-05 16:46:41 +0300

    a
    a

--- script.js
+++ script.js
@@ -1,104 +1,25 @@
-var o = $(".card");
-$("#top").on("mousemove", function (t) {
-    var e = -($(window).innerWidth() / 2 - t.pageX) / 30,
-        n = ($(window).innerHeight() / 2 - t.pageY) / 10;
-    o.attr("style", "transform: rotateY(" + e + "deg) rotateX(" + n + "deg);-webkit-transform: rotateY(" + e + "deg) rotateX(" + n + "deg);-moz-transform: rotateY(" + e + "deg) rotateX(" + n + "deg)")
-})
-// component.js
-export function createDivComponent() {
-  const div = document.createElement('div');
-  div.className = 'my-component';
-  div.textContent = 'This is a modular component';
-  return div;
+let scrollDirection = 1;
+let lastScrollY = window.scrollY;
+
+function reverseAnimation() {
+    const scrollingText = document.getElementById('scrolling-text');
+    const computedStyle = window.getComputedStyle(scrollingText);
+    const matrix = new DOMMatrixReadOnly(computedStyle.transform);
+    const currentTranslateX = matrix.m41; // get current translateX value
+
+    scrollingText.style.animation = 'none'; // pause the animation
+    scrollingText.style.transform = `translateX(${currentTranslateX}px)`; // set current position
+    setTimeout(() => {
+        scrollingText.style.animation = `scroll 10s linear infinite ${scrollDirection === 1 ? 'normal' : 'reverse'}`;
+    }, 10);
 }
-document.addEventListener('DOMContentLoaded', () => {
-    const app = document.getElementById('app');
-    import('./component.js').then(({ createDivComponent }) => {
-      const component = createDivComponent();
-      app.appendChild(component);
-  
-      setTimeout(() => {
-        const loadingElement = document.getElementById('loading');
-        if (loadingElement) {
-          loadingElement.remove();
-        }
-      }, 2000); // Remove the loading text after 2 seconds
-    });
-  });
-  
-  function Ticker( elem ) {
-    elem.lettering();
-    this.done = false;
-    this.cycleCount = 5;
-    this.cycleCurrent = 0;
-    this.chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_=+{}|[]\\;\':"<>?,./`~'.split('');
-    this.charsCount = this.chars.length;
-    this.letters = elem.find( 'span' );
-    this.letterCount = this.letters.length;
-    this.letterCurrent = 0;
-  
-    this.letters.each( function() {
-      var $this = $( this );
-      $this.attr( 'data-orig', $this.text() );
-      $this.text( '-' );
-    });
-  }
-  
-  Ticker.prototype.getChar = function() {
-    return this.chars[ Math.floor( Math.random() * this.charsCount ) ];
-  };
-  
-  Ticker.prototype.reset = function() {
-    this.done = false;
-    this.cycleCurrent = 0;
-    this.letterCurrent = 0;
-    this.letters.each( function() {
-      var $this = $( this );
-      $this.text( $this.attr( 'data-orig' ) );
-      $this.removeClass( 'done' );
-    });
-    this.loop();
-  };
-  
-  Ticker.prototype.loop = function() {
-    var self = this;
-  
-    this.letters.each( function( index, elem ) {
-      var $elem = $( elem );
-      if( index >= self.letterCurrent ) {
-        if( $elem.text() !== ' ' ) {
-          $elem.text( self.getChar() );
-          $elem.css( 'opacity', Math.random() );
-        }
-      }
-    });
-  
-    if( this.cycleCurrent < this.cycleCount ) {
-      this.cycleCurrent++;
-    } else if( this.letterCurrent < this.letterCount ) {
-      var currLetter = this.letters.eq( this.letterCurrent );
-      this.cycleCurrent = 0;
-      currLetter.text( currLetter.attr( 'data-orig' ) ).css( 'opacity', 1 ).addClass( 'done' );
-      this.letterCurrent++;
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > lastScrollY) {
+        scrollDirection = -1;
     } else {
-      this.done = true;
+        scrollDirection = 1;
     }
-  
-    if( !this.done ) {
-      requestAnimationFrame( function() {
-        self.loop();
-      });
-    } else {
-      setTimeout( function() {
-        self.reset();
-      }, 750 );
-    }
-  };
-  
-  $words = $( '.word' );
-  
-  $words.each( function() {
-    var $this = $( this ),
-      ticker = new Ticker( $this ).reset();
-    $this.data( 'ticker', ticker  );
-  });
+    reverseAnimation();
+    lastScrollY = window.scrollY;
+});
